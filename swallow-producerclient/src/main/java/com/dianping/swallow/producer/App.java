eef058b104164ce0f974a8ba5d542a1eb32c7e0d
wukezhu <kezhu.wu@dianping.com>
2012-06-04 14:47:12 +0800

    去掉多余的目录swallow-0.6.0

---
+++ swallow-producerclient/src/main/java/com/dianping/swallow/producer/App.java
@@ -0,0 +1,13 @@
+package com.dianping.swallow.producer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
