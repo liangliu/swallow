ea5d870c7455e331983021d70287ba66e0513f92
wukezhu <kezhu.wu@dianping.com>
2012-06-04 13:50:52 +0800

    初始化maven工程

---
+++ swallow-0.6.0/swallow-producerserver/src/main/java/com/dianping/swallow/producerserver/App.java
@@ -0,0 +1,13 @@
+package com.dianping.swallow.producerserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
