1971e0fe74935e8a82cb6f37b2d8214e3993ca4a
tong.song <tong.song@tech-songtong.dianpingoa.com>
2012-06-05 10:47:07 +0800

    初次迁移

---
+++ swallow-producerclient/src/main/java/com/dianping/swallow/producer/PktStringMessage.java
@@ -0,0 +1,48 @@
+/**
+ * Project: swallow-client
+ * 
+ * File Created at 2012-5-28
+ * $Id$
+ * 
+ * Copyright 2010 dianping.com.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Dianping Company. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with dianping.com.
+ */
+package com.dianping.swallow.producer;
+
+import com.dianping.swallow.producer.Destination;
+
+/**
+ * TODO Comment of PktMessage
+ * @author tong.song
+ *
+ */
+public final class PktStringMessage extends Packet{
+	private Destination dest;
+	private String content;
+	private int ackNum;
+
+	public PktStringMessage(Destination dest, String content, int ackNum) {
+		this.setPacketType(PacketType.STRING_MSG);
+		this.dest = dest;
+		this.setContent(content);
+		this.ackNum = ackNum;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public int getAckNum(){
+		return ackNum;
+	}
+}
