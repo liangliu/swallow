c61ae74b6117af90998086ae2ca0146f4c89b017
zhang.yu <zhang.yu@tech-w-zhangyu.dianpingoa.com>
2012-06-07 10:55:18 +0800

    DAO的添加

---
+++ swallow-common/src/main/java/com/dianping/swallow/common/dao/CounterDAO.java
@@ -0,0 +1,9 @@
+package com.dianping.swallow.common.dao;
+
+import org.bson.types.BSONTimestamp;
+
+public interface CounterDAO {
+	
+	BSONTimestamp getMaxTimeStamp(String topicId, String consumerId);
+	
+}
---
+++ swallow-common/src/main/java/com/dianping/swallow/common/dao/impl/TopicDAOImpl.java
@@ -0,0 +1,10 @@
+package com.dianping.swallow.common.dao.impl;
+
+
+import com.dianping.swallow.common.dao.TopicDAO;
+
+public class TopicDAOImpl implements TopicDAO {
+
+
+
+}
