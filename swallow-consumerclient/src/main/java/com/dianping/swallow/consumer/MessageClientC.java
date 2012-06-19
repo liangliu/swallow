ed233541f076784487ef60d65c1e312c942019de
zhang.yu <zhang.yu@tech-w-zhangyu.dianpingoa.com>
2012-06-19 15:36:01 +0800

    consumerClient的开发

--- swallow-consumerclient/src/main/java/com/dianping/swallow/consumer/MessageClientC.java
+++ swallow-consumerclient/src/main/java/com/dianping/swallow/consumer/MessageClientC.java
@@ -1,29 +1,32 @@
 package com.dianping.swallow.consumer;
 
 import java.net.InetSocketAddress;
-import java.util.concurrent.Executors;
+
 import org.jboss.netty.bootstrap.ClientBootstrap;
 import org.jboss.netty.channel.ChannelFuture;
-import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
 
 
 public class MessageClientC {
 	//应该都是取自于lion
-	static String host = "127.0.0.1";
-	static int marsterPort = 8081;
-	static int slavePort = 8082;
-    public static void main(String[] args) throws Exception {        
-        ClientBootstrap bootstrap = new ClientBootstrap(
-                new NioClientSocketChannelFactory(
-                        Executors.newCachedThreadPool(),
-                        Executors.newCachedThreadPool()));
-        // Set up the event pipeline factory.
-        bootstrap.setPipelineFactory(new MessageClientPipelineFactoryC());
-        // Start the connection attempt.
-        ChannelFuture future = bootstrap.connect(new InetSocketAddress(host, marsterPort));
-        // Wait until the connection is closed or the connection attempt fails.
-        future.getChannel().getCloseFuture().awaitUninterruptibly();
-        // Shut down thread pools to exit.
-        bootstrap.releaseExternalResources();
+	public static String host = "127.0.0.1";
+	public static int marsterPort = 8081;
+	public static int slavePort = 8082;
+    public static void main(String[] args) throws Exception {
+    	
+    	ConsumerClient cClient = new ConsumerClient();
+	   	cClient.init();
+	   	ClientBootstrap bootstrap = cClient.getBootstrap();
+	   	ConSlaveThread slave = new ConSlaveThread();
+    	slave.setBootstrap(bootstrap);
+	   	Thread slaveThread = new Thread(slave);
+	   	slaveThread.start();
+	   	while(true){
+	   		synchronized(bootstrap){
+		   		ChannelFuture future = bootstrap.connect(new InetSocketAddress(host, marsterPort));
+		   		future.getChannel().getCloseFuture().awaitUninterruptibly();//等待channel关闭，否则一直阻塞!	   		
+		   	}
+	   		Thread.sleep(1000);//TODO 配置变量
+	   	}	   		   		   	
+    	
     }
 }
--- swallow-consumerclient/src/main/java/com/dianping/swallow/consumer/netty/MessageEncoder.java
+++ swallow-consumerclient/src/main/java/com/dianping/swallow/consumer/netty/MessageEncoder.java
@@ -1,4 +1,4 @@
-package com.dianping.swallow.consumer;
+package com.dianping.swallow.consumer.netty;
 
 
 
