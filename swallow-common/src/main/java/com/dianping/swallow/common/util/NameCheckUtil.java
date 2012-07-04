dc3dd179461dbf11e4adf7457bc3f1641a71a089
wukezhu <kezhu.wu@dianping.com>
2012-07-04 10:30:11 +0800

    命名限制的长度由20改为30，因为现有名字有20多长度的。

--- swallow-common/src/main/java/com/dianping/swallow/common/util/NameCheckUtil.java
+++ swallow-common/src/main/java/com/dianping/swallow/common/util/NameCheckUtil.java
@@ -20,7 +20,7 @@ public class NameCheckUtil {
     * 判定topicName是否合法
     * 
     * <pre>
-    * topicName由字母,数字,小数点“.”,减号“-”和下划线“_”构成，只能以字母开头，长度为2到20。
+    * topicName由字母,数字,小数点“.”,减号“-”和下划线“_”构成，只能以字母开头，长度为2到30。
     * </pre>
     * 
     * @param topicName
@@ -30,7 +30,7 @@ public class NameCheckUtil {
       if (topicName == null || topicName.length() == 0) {
          return false;
       }
-      if (topicName.matches("[a-z|A-Z][a-z|A-Z|_|.|-|0-9]{1,19}"))
+      if (topicName.matches("[a-z|A-Z][a-z|A-Z|_|.|-|0-9]{1,29}"))
          return true;
       return false;
    }
@@ -39,7 +39,7 @@ public class NameCheckUtil {
     * 判定consumerId是否合法
     * 
     * <pre>
-    * consumerId由字母,数字,小数点“.”,减号“-”和下划线“_”构成，只能以字母开头，长度为2到20。
+    * consumerId由字母,数字,小数点“.”,减号“-”和下划线“_”构成，只能以字母开头，长度为2到30。
     * </pre>
     * 
     * @param consumerId
@@ -49,7 +49,7 @@ public class NameCheckUtil {
       if (consumerId == null || consumerId.length() == 0) {
          return false;
       }
-      if (consumerId.matches("[a-z|A-Z][a-z|A-Z|_|.|-|0-9]{1,19}"))
+      if (consumerId.matches("[a-z|A-Z][a-z|A-Z|_|.|-|0-9]{1,29}"))
          return true;
       return false;
    }
@@ -65,6 +65,6 @@ public class NameCheckUtil {
       System.out.println(isTopicNameValid("a"));//false
       System.out.println(isTopicNameValid("_"));//false
       System.out.println(isTopicNameValid("3"));//false
-      System.out.println(isTopicNameValid("a12345678901234567890"));//false
+      System.out.println(isTopicNameValid("a123456789012345678901234567890"));//false
    }
 }
