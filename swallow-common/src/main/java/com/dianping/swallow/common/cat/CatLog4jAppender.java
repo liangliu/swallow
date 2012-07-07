a3646a1de52f1a6ad973ea076ee375d6ad45b25b
wukezhu <kezhu.wu@dianping.com>
2012-07-07 16:15:05 +0800

    修改文案。

--- swallow-common/src/main/java/com/dianping/swallow/common/cat/CatLog4jAppender.java
+++ swallow-common/src/main/java/com/dianping/swallow/common/cat/CatLog4jAppender.java
@@ -11,7 +11,8 @@ import com.dianping.cat.message.Transaction;
 public class CatLog4jAppender extends AppenderSkeleton {
    @Override
    protected void append(LoggingEvent event) {
-      Transaction t = Cat.getProducer().newTransaction("errorlog", event.getLevel().toString());
+      String level = event.getLevel().toString();
+      Transaction t = Cat.getProducer().newTransaction("log", level);
       try {
          ThrowableInformation throwableInformation = event.getThrowableInformation();
          if (throwableInformation != null) {
@@ -19,7 +20,7 @@ public class CatLog4jAppender extends AppenderSkeleton {
          } else {
             String msg = "[thread:" + event.getThreadName() + "][" + event.getLocationInformation().getClassName()
                   + " line:" + event.getLocationInformation().getLineNumber() + "] " + event.getRenderedMessage();
-            Cat.getProducer().logEvent("errorlog", "errorlog", Event.SUCCESS, msg);
+            Cat.getProducer().logEvent("log", level, Event.SUCCESS, msg);
          }
          t.setStatus(Transaction.SUCCESS);
       } catch (Exception e) {
