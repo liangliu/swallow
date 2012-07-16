96a4eb4ab4dea2023a46a1d5dfb7a304a4b2bae8
zhang.yu <zhang.yu@tech-w-zhangyu.dianpingoa.com>
2012-07-16 17:07:08 +0800

    把listener设置成bean

--- swallow-example/src/main/java/com/dianping/swallow/example/consumer/spring/TestNonDurableConsumer.java
+++ swallow-example/src/main/java/com/dianping/swallow/example/consumer/spring/TestNonDurableConsumer.java
@@ -3,9 +3,7 @@ package com.dianping.swallow.example.consumer.spring;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.dianping.swallow.common.message.Message;
 import com.dianping.swallow.consumer.Consumer;
-import com.dianping.swallow.consumer.MessageListener;
 
 public class TestNonDurableConsumer {
 
@@ -15,21 +13,6 @@ public class TestNonDurableConsumer {
    public static void main(String[] args) {
       ApplicationContext ctx = new ClassPathXmlApplicationContext(new String[] { "applicationContext-consumer.xml" });
       final Consumer ConsumerClient = (Consumer) ctx.getBean("nonDurableConsumerClient");
-      ConsumerClient.setListener(new MessageListener() {
-
-         @Override
-         public void onMessage(Message swallowMessage) {
-            //用户得到SwallowMessage
-
-            System.out.println(swallowMessage.getMessageId() + ":" + swallowMessage.getContent());
-            try {
-               Thread.sleep(100);
-            } catch (InterruptedException e) {
-               // TODO Auto-generated catch block
-               e.printStackTrace();
-            }
-         }
-      });
       ConsumerClient.start();
 
    }
