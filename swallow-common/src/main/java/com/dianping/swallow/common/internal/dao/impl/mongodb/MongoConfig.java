49de157337915bc1fb185ff3a13525d45f1aaf47
wukezhu <kezhu.wu@dianping.com>
2012-07-20 15:28:47 +0800

    2个server去掉swallow-mongo.properties；调整mongo参数

--- swallow-common/src/main/java/com/dianping/swallow/common/internal/dao/impl/mongodb/MongoConfig.java
+++ swallow-common/src/main/java/com/dianping/swallow/common/internal/dao/impl/mongodb/MongoConfig.java
@@ -21,15 +21,15 @@ class MongoConfig {
 
    // local config(mongo server options)
    private boolean             slaveOk                                      = true;
-   private boolean             socketKeepAlive                              = false;
-   private int                 socketTimeout                                = 500;
+   private boolean             socketKeepAlive                              = true;
+   private int                 socketTimeout                                = 1000;
    private int                 connectionsPerHost                           = 30;
    private int                 threadsAllowedToBlockForConnectionMultiplier = 50;
-   private int                 w                                            = 0;
+   private int                 w                                            = 1;
    private int                 wtimeout                                     = 500;
    private boolean             fsync                                        = false;
-   private int                 connectTimeout                               = 500;
-   private int                 maxWaitTime                                  = 1000 * 60 * 2;
+   private int                 connectTimeout                               = 2000;
+   private int                 maxWaitTime                                  = 2000;
    private boolean             autoConnectRetry                             = false;
    private boolean             safe                                         = true;
 
