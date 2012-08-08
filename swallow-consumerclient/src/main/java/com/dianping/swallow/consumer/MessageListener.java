08ad34b5514043bf4862086e9cf2151442a5d8d8
wukezhu <kezhu.wu@dianping.com>
2012-08-08 12:52:24 +0800

    Consumer的onMessage添加retry机制

--- swallow-consumerclient/src/main/java/com/dianping/swallow/consumer/MessageListener.java
+++ swallow-consumerclient/src/main/java/com/dianping/swallow/consumer/MessageListener.java
@@ -8,7 +8,15 @@ public interface MessageListener {
     * 消息处理回调方法
     * 
     * @param msg
+    * @throws BackoutMessageException
+    *            当consumer无法处理Message且希望重试该消息时，可以抛出该异常，Swallow接收到该异常会使用重试策略（见
+    *            {@link com.dianping.swallow.consumer.ConsumerConfig} 的
+    *            delayBaseOnBackoutMessageException
+    *            ，delayUpperboundOnBackoutMessageException
+    *            ，retryCountOnBackoutMessageException选项）进行重试，即
+    *            {@link com.dianping.swallow.consumer.MessageListener}
+    *            的onMessage方法会被重复调用
     */
-   void onMessage(Message msg);
+   void onMessage(Message msg) throws BackoutMessageException;
 
 }
