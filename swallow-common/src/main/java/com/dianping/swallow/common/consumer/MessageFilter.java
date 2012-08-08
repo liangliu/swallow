08aaee5ba35c559af561e91e51026e480ab40e25
wukezhu <kezhu.wu@dianping.com>
2012-08-08 13:07:09 +0800

    添加注释

--- swallow-common/src/main/java/com/dianping/swallow/common/consumer/MessageFilter.java
+++ swallow-common/src/main/java/com/dianping/swallow/common/consumer/MessageFilter.java
@@ -3,25 +3,35 @@ package com.dianping.swallow.common.consumer;
 import java.io.Serializable;
 import java.util.Set;
 
+/**
+ * 该类用于作消息过滤。<br>
+ * 使用<code>createInSetMessageFilter(Set&lt;String&gt; matchTypeSet)</code>
+ * 构造实例时，参数matchTypeSet指定了Consumer只消费“Message.type属性包含在matchTypeSet中”的消息
+ * 
+ * @author kezhu.wu
+ */
 public final class MessageFilter implements Serializable {
-   
-   private static final long serialVersionUID = 5643819915814285301L;
-   
-   public final static MessageFilter AllMatchFilter = new MessageFilter(FilterType.AllMatch, null);
 
-   public enum FilterType {AllMatch, InSet};
-   
-   private FilterType type;
+   private static final long         serialVersionUID = 5643819915814285301L;
+
+   public final static MessageFilter AllMatchFilter   = new MessageFilter(FilterType.AllMatch, null);
+
+   public enum FilterType {
+      AllMatch,
+      InSet
+   };
+
+   private FilterType  type;
    private Set<String> param;
-   
+
    private MessageFilter() {
    }
-   
+
    private MessageFilter(FilterType type, Set<String> param) {
       this.type = type;
       this.param = param;
    }
-   
+
    public static MessageFilter createInSetMessageFilter(Set<String> matchTypeSet) {
       return new MessageFilter(FilterType.InSet, matchTypeSet);
    }
@@ -38,5 +48,5 @@ public final class MessageFilter implements Serializable {
    public String toString() {
       return "MessageFilter [param=" + param + ", type=" + type + "]";
    }
-   
+
 }
