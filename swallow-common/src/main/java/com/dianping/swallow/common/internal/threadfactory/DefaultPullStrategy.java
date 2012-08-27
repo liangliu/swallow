9d6b573fbc9f84d094f4fd9784ae4664616b1832
wukezhu <kezhu.wu@dianping.com>
2012-08-27 11:33:57 +0800

    DefaultPullStrategy的log改为使用slf4j

--- swallow-common/src/main/java/com/dianping/swallow/common/internal/threadfactory/DefaultPullStrategy.java
+++ swallow-common/src/main/java/com/dianping/swallow/common/internal/threadfactory/DefaultPullStrategy.java
@@ -1,59 +1,43 @@
-/**
- * Project: ${swallow-client.aid}
- * 
- * File Created at 2011-7-29
- * $Id$
- * 
- * Copyright 2011 dianping.com.
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of
- * Dianping Company. ("Confidential Information").  You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with dianping.com.
- */
 package com.dianping.swallow.common.internal.threadfactory;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * 
  * 失败时累加failCnt，sleep(min(delayBase * failCnt, delayUpperbound))，成功时清零failCnt
  * 
  * @author marsqing
- * 
  */
 public class DefaultPullStrategy implements PullStrategy {
-	
-	private static Logger log = Logger.getLogger(DefaultPullStrategy.class);
 
-	private int failCnt = 0;
-	private final int delayBase;
-	private final int delayUpperbound;
+   private static Logger log     = LoggerFactory.getLogger(DefaultPullStrategy.class);
 
-	public DefaultPullStrategy(int delayBase, int delayUpperbound) {
-		this.delayBase = delayBase;
-		this.delayUpperbound = delayUpperbound;
-	}
+   private int           failCnt = 0;
+   private final int     delayBase;
+   private final int     delayUpperbound;
 
-	@Override
-	public long fail(boolean shouldSleep) throws InterruptedException {
-		failCnt++;
-		long sleepTime = (long)failCnt * delayBase;
-		sleepTime = sleepTime > delayUpperbound ? delayUpperbound : sleepTime;
-		if(shouldSleep) {
-		   if(log.isDebugEnabled()) {
-	         log.debug("sleep " + sleepTime + " at " + this.getClass().getSimpleName());
-	      }
-		   Thread.sleep(sleepTime);
-		}
-		return sleepTime;
-	}
+   public DefaultPullStrategy(int delayBase, int delayUpperbound) {
+      this.delayBase = delayBase;
+      this.delayUpperbound = delayUpperbound;
+   }
 
-	@Override
-	public void succeess() {
-		failCnt = 0;
-	}
+   @Override
+   public long fail(boolean shouldSleep) throws InterruptedException {
+      failCnt++;
+      long sleepTime = (long) failCnt * delayBase;
+      sleepTime = sleepTime > delayUpperbound ? delayUpperbound : sleepTime;
+      if (shouldSleep) {
+         if (log.isDebugEnabled()) {
+            log.debug("sleep " + sleepTime + " at " + this.getClass().getSimpleName());
+         }
+         Thread.sleep(sleepTime);
+      }
+      return sleepTime;
+   }
+
+   @Override
+   public void succeess() {
+      failCnt = 0;
+   }
 
 }
