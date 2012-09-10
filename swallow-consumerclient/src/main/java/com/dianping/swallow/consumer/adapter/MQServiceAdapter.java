27e41eb89135f33259843f04748cc72a56efae01
tong.song <tong.song@tech-songtong.dianpingoa.com>
2012-09-10 15:43:17 +0800

    实现接口

--- swallow-consumerclient/src/main/java/com/dianping/swallow/consumer/adapter/MQServiceAdapter.java
+++ swallow-consumerclient/src/main/java/com/dianping/swallow/consumer/adapter/MQServiceAdapter.java
@@ -1,5 +1,42 @@
 package com.dianping.swallow.consumer.adapter;
 
-public class MQServiceAdapter {
+import java.util.Map;
+
+import com.dianping.swallow.Destination;
+import com.dianping.swallow.MQService;
+import com.dianping.swallow.MessageConsumer;
+import com.dianping.swallow.MessageProducer;
+
+public class MQServiceAdapter implements MQService{
+
+   @Override
+   public MessageProducer createProducer(Destination dest, Map<ProducerOptionKey, Object> options) {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public MessageProducer createProducer(Destination dest) {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public MessageConsumer createConsumer(Destination dest, Map<ConsumerOptionKey, Object> options) {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public MessageConsumer createConsumer(Destination dest) {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public void close() {
+      // TODO Auto-generated method stub
+      
+   }
 
 }
--- swallow-consumerclient/src/main/java/com/dianping/swallow/consumer/adapter/MessageConsumerAdapter.java
+++ swallow-consumerclient/src/main/java/com/dianping/swallow/consumer/adapter/MessageConsumerAdapter.java
@@ -1,6 +1,24 @@
 package com.dianping.swallow.consumer.adapter;
 
-public class MessageConsumerAdapter {
+import com.dianping.swallow.MessageConsumer;
+import com.dianping.swallow.MessageListener;
 
+public class MessageConsumerAdapter implements MessageConsumer{
    
+   public MessageConsumerAdapter(){
+      
+   }
+
+   @Override
+   public void setMessageListener(MessageListener listener) {
+      // TODO Auto-generated method stub
+      
+   }
+
+   @Override
+   public void close() {
+      // TODO Auto-generated method stub
+      
+   }
+  
 }
